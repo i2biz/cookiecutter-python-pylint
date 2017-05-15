c3f6b921bcef25616f0c2612f43dd6a3bcde88d5
Jacek Bzdak <jbzdak@gmail.com>
2017-05-15 19:01:26 +0200

    Added pytext config and requirements.

--- {{cookiecutter.project_slug}}/tasks.py
+++ {{cookiecutter.project_slug}}/tasks.py
@@ -2,6 +2,12 @@
 
 from invoke import task
 
+
+@task
+def pep8(ctx):
+  ctx.run("pep8 {{cookiecutter.project_slug}} {{cookiecutter.project_slug}}_test")
+
+
 @task
 def lint(ctx):
   ctx.run("pylint {{cookiecutter.project_slug}} {{cookiecutter.project_slug}}_test")
@@ -12,7 +18,7 @@ def test(ctx):
   ctx.run("py.test --cov {{cookiecutter.project_slug}}")
 
 
-@task(pre=[test, lint])
+@task(pre=[test, pep8, lint])
 def check(ctx):
   pass
 
