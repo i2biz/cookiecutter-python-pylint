3b0e9c62f11a8156bc519e4832e7d78abdadfc27
Jacek Bzdak <jbzdak@gmail.com>
2018-06-26 19:01:54 +0200

    Removed pip.req requirement from the setup file.

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -3,18 +3,20 @@
 from setuptools import find_packages, setup
 import pathlib
 import os
-from pip.req import parse_requirements
-from pip.download import PipSession
 
 MAIN_DIR = pathlib.Path(__file__).absolute().parent
 
 
-def get_requirements():
-  pip_session = PipSession()
-  return parse_requirements(
-      str(MAIN_DIR / 'requirements' / 'base.txt'),
-      session=pip_session
-  )
+def get_packages():
+  base_file = MAIN_DIR / 'requirements' / 'base.in'
+  response = []
+
+  with base_file.open('r') as file:
+    for line in file:
+      line = line.strip()
+      if not line.startswith("#"):
+        response.append(line)
+  return response
 
 packages = find_packages(
   str(MAIN_DIR),
@@ -24,8 +26,6 @@ packages = find_packages(
 
 # Did I mention that setup.py is not finest piece of software on earth.
 # For this to work when installed you'll need to enumerate all template and static file.
-
-
 def read_dir(package: str, directory: str):
   package_root = os.path.abspath(package.replace(".", "/"))
   directory = os.path.join(package_root, directory)
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     author='{{ cookiecutter.full_name }}',
     author_email='{{ cookiecutter.email }}',
     description='{{cookiecutter.project_short_description}}',
-    install_requires=[str(req.req) for req in get_requirements()],
+    install_requires=get_packages(),
     package_data={
       package: [] +
         read_dir(package, "static") +
